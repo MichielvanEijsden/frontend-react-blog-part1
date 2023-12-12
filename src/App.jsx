dcd07406f038b7601bc5307a30d637585b773e2d
MichielvanEijsden <143597805+MichielvanEijsden@users.noreply.github.com>
2023-12-12 14:42:58 +0100

    eerste push

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 import logo from './assets/logo-white.png'
+import {Routes} from "react-router-dom";
 
 function App() {
     return (
         <div className="page-container">
             <img src={logo} alt="Company logo"/>
             <h1>Begin hier met het maken van jouw blog-applicatie!</h1>
+    <Routes/>
         </div>
     )
 }
