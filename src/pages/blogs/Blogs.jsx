5866f445b4c26e84456c0d9b7449501e0cd4bd0b
MichielvanEijsden <143597805+MichielvanEijsden@users.noreply.github.com>
2023-12-21 22:15:20 +0100

    all blogs with GET

--- src/pages/blogs/Blogs.jsx
+++ src/pages/blogs/Blogs.jsx
@@ -1,16 +1,37 @@
-import posts from '../../constants/data.json'
+
 import {Link} from "react-router-dom";
 import './Blogs.css'
+import {useEffect, useState} from "react";
+import axios from "axios";
+
 
 
 function Blogs() {
 
+    const [allBlogs,setAllBlogs]=useState([])
+    const [error,setError]=useState()
+
+    useEffect(() => {
+        fetchAllBlogs()
+    }, []);
+    async function fetchAllBlogs() {
+        try {
+            const result = await axios.get('http://localhost:3000/posts')
+            console.log(result.data);
+            setAllBlogs(result.data)
+
+        } catch (e) {
+            setError(e)
+            console.error(error);
+        }
+    }
+
     return (
         <section className="overview-section outer-content-container">
             <div className="inner-content-container">
-                <h1>Bekijk alle {posts.length} posts op het platform</h1>
+                <h1>Bekijk alle {allBlogs.length} posts op het platform</h1>
                 <ul className="post-list">
-                    {posts.map((post) => {
+                    {allBlogs.map((post) => {
                         return <li key={post.id} className="post-item">
                             <h2 className="post-title"><Link to={`/Blogs/${post.id}`}>{post.title}</Link> ({post.author})
                             </h2>
